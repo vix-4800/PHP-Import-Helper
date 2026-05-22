a668b35ce51f22cfd298ce2f597c336d2133575d
Anton Vix <Gordan.ei@protonmail.ch>
2026-05-23 02:46:30 +0400

    add PhpAstParser class for parsing PHP code using php-parser

---
+++ src/core/phpParser.ts
@@ -0,0 +1,20 @@
+declare function require(name: string): unknown;
+
+export class PhpAstParser {
+    private readonly engine: { parseCode(code: string, filename?: string): unknown };
+
+    public constructor() {
+        const Parser = require('php-parser') as {
+            Engine: new (options: unknown) => { parseCode(code: string, filename?: string): unknown };
+        };
+
+        this.engine = new Parser.Engine({
+            parser: { extractDoc: true, php7: true, suppressErrors: true },
+            ast: { withPositions: true },
+        });
+    }
+
+    public parse(code: string, filename = 'document.php'): unknown {
+        return this.engine.parseCode(code, filename);
+    }
+}
